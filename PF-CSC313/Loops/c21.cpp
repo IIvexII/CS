f756dce5a15d62f6de558a4995d954514b13b733
IIvexII <mrh4ck3r1738@gmail.com>
2021-01-05 21:22:51 +0500

    Create c21.cpp

---
+++ PF-CSC313/Loops/c21.cpp
@@ -0,0 +1,26 @@
+/*
+    Date: 5-1-2021
+    Purpose: Write a program that display the following:
+            1 = 1
+            1 + 2 = 3
+            1 + 2 + 3 = 6
+            1 + 2 + 3 + 4 = 10
+            1 + 2 + 3 + 4 + 5 = 15
+    Author: Zafeer
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum;
+    for(int i=1; i<=5; i++){
+        sum=0;
+        for (int j=1;j<=i; j++){
+            sum+=j;
+            cout << ((j>1)? " + " : "") << j;
+        }
+        cout << " = " << sum << endl;
+    }
+
+    return 0;
+}
