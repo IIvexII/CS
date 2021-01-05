ce968f99d25349278193a3430a04573f161f64eb
IIvexII <mrh4ck3r1738@gmail.com>
2021-01-05 21:26:13 +0500

    Update c22.cpp

---
+++ PF-CSC313/Loops/c22.cpp
@@ -0,0 +1,22 @@
+/*
+    Date: 5-1-2020
+    Purpose: Write a program that display the following shape using
+             nested loops. The outer loop should b for loop and inner
+             loop should be while loop.
+             * * * * * * *
+             * * * * * *
+             * * * * *
+             * * * * 
+             * * *
+             * *
+             *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
