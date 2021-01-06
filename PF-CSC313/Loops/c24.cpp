c20871833551e5844e5fc3d822eea1a5cd569df0
IIvexII <mrh4ck3r1738@gmail.com>
2021-01-06 18:30:55 +0500

    Update c24.cpp

--- PF-CSC313/Loops/c24.cpp
+++ PF-CSC313/Loops/c24.cpp
@@ -13,7 +13,7 @@ int main(){
 
     for(int i=1;i<=4;i++){
         for(int j=1; j<=4; j++){
-            if(j>=5-i)
+            if(j>=5-i && j<=4)
                 cout << " *" ;
 
             else
