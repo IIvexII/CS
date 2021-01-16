6947ac06ccc6f76be8394a8d892829279954ceed
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2021-01-16 10:35:17 +0500

    Update c28.cpp

--- PF-CSC313/Loops/c28.cpp
+++ PF-CSC313/Loops/c28.cpp
@@ -14,16 +14,13 @@
 using namespace std;
 
 int main(){
-    for(int i=1;i<=8;i++){
-        for(int j=1; j<=8; j++){
-            if(i<8)
-                if(j==1||j==i)
-                    cout << " *";
-                else
-                    cout << "  ";
-            else
-                if(j<=8 && j>=1)
-                    cout << " *";
+    for(int i=1;i<=6;i++){
+        for(int j=1; j<=6; j++){
+            if(i==6)
+                cout << "* ";
+            else 
+                if(j==7-i || j==6)
+                    cout << "* ";
                 else
                     cout << "  ";
         }
